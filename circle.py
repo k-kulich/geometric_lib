e7d62c2213e8155c3009846673d7798ab2169eef
Катарина Воронова <katarinavoronova@MacBook-Air-Katarina.local>
2024-10-23 08:52:28 +0300

    added_documentation

--- circle.py
+++ circle.py
@@ -2,9 +2,19 @@ import math
 
 
 def area(r):
+    """
+    Возвращает площадь окружности на основе переданного ему радиуса.
+        param: a (int|float) - радиус окружности;
+    return: square (float) - площадь квадрата.
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """
+    Возвращает периметр окружности на основе переданного ему радиуса.
+        param: a (int|float) - радиус окружности;
+    return: square (float) - площадь квадрата.
+    """
     return 2 * math.pi * r
 
--- square.py
+++ square.py
@@ -1,7 +1,17 @@
 
 def area(a):
+    """
+    Возвращает площадь квадрата на основе переданной ему стороны.
+        param: a (int|float) - сторона квадрата;
+    return: square (int|float) - площадь квадрата.
+    """ 
     return a * a
 
 
 def perimeter(a):
+    """
+    Возвращает периметр квадрата на основе переданной ему стороны.
+        param: a (int|float) - сторона квадрата;
+    return: perimeter (int|float) - периметр квадрата.
+    """
     return 4 * a
